14693a31bec1d116af7facde54e472cf7ecca120
Saylion <126657247+Saylion@users.noreply.github.com>
2023-10-08 07:23:17 +0700

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -28,10 +28,15 @@ if __name__ == '__main__':
        # print(f'Downloading {model}...')
         dl_model(PRETRAINED_V1_DOWNLOAD_LINK, model, pretrained_v2_models_dir)
 
-    rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
-    for model in rvc_model_names:
+    hubert_model_names = ['hubert_base.pt',]
+    for model in hubert_model_names:
         print(f'Downloading {model}...')
-        dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
+        dl_model(HUBERT_DOWNLOAD_LINK, model, hubert_models_dir)
+
+    rmvpe_models_names = ['rmvpe.pt']
+    for model in rmvpe_model_names:
+        print(f'Downloading {model}...')
+        dl_model(RMVPE_DOWNLOAD_LINK, model, rmvpe_models_dir)
 
     pretrained_v1_model_names = ['D32k.pth', 'D40k.pth', 'D48k.pth', 'G32k.pth', 'G40k.pth', 'G48k.pth', 'f0D32k.pth', 'f0D40k.pth', 'f0D48k.pth', 'f0G32k.pth', 'f0G40k.pth', 'f0G48k.pth']
     print(f'Downloading pretrained models...')
