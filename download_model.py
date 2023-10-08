120ef168021a6bb54f4f727e5b58780c9952b9cf
Saylion <126657247+Saylion@users.noreply.github.com>
2023-10-08 07:24:44 +0700

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -8,7 +8,7 @@ PRETRAINED_V1_DOWNLOAD_LINK = 'https://huggingface.co/Salmizu/Pretrained/resolve
 PRETRAINED_V2_DOWNLOAD_LINK = 'https://huggingface.co/Salmizu/Pretrained_v2/resolve/main/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-hubert_models_dir = BASE_DIR / assets / 'hubert'
+hubert_models_dir = BASE_DIR /assets/ 'hubert'
 rmvpe_models_dir = BASE_DIR / assets / 'rmvpe'
 pretrained_v1_models_dir = BASE_DIR / assets / 'pretrained'
 pretrained_v2_models_dir = BASE_DUR / assets / 'pretrained_v2'
